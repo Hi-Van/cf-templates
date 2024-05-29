a01e339382db1ef7232a862cd17a829a80c7e77a
Hi-Van <sirisoukvanouphon@gmail.com>
2024-05-29 17:08:52 -0500

    add starter template

---
+++ general/starter.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+This command produces a binary file test from the source code test.cpp:
+g++ -std=c++11 -O2 -Wall test.cpp -o test
+*/
+
+int main() {
+    // code here
+}
